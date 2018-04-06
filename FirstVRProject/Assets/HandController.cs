d05e9b6e9e8e38bec2ad9b8ca58b032b280cffd7
Florent <boissy.florent@gmail.com>
2018-04-06 08:57:12 +0200

    On avance

--- FirstVRProject/Assets/HandController.cs
+++ FirstVRProject/Assets/HandController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class HandController : MonoBehaviour {
+    
+    public Transform takenPos;
+    public bool canCatch = false;
+    public GameObject catchable;
 
 	// Use this for initialization
 	void Start () {
@@ -11,6 +15,28 @@ public class HandController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(canCatch == true && catchable != null)
+        {
+            if (Input.GetKey(KeyCode.Mouse0))
+            {
+                catchable.gameObject.transform.SetParent(gameObject.transform);
+                catchable.GetComponent<Collider>().isTrigger = true;
+                catchable.transform.position = takenPos.position;
+                catchable.transform.rotation = gameObject.transform.rotation;
+            }            
+        }
 	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        canCatch = true;
+        catchable = other.gameObject;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        canCatch = false;
+        catchable = null;
+    }
+
 }
