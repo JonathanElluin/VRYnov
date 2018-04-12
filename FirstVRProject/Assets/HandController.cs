8308cb7bcbcefb8d67f8f2f3094d334fb1a834f9
Florent <boissy.florent@gmail.com>
2018-04-12 13:04:32 +0200

    commit

--- FirstVRProject/Assets/HandController.cs
+++ FirstVRProject/Assets/HandController.cs
@@ -1,12 +1,17 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class HandController : MonoBehaviour {
     
     public Transform takenPos;
+    public Transform HandHolder;
+    private IUsable myUsableItem;
     public bool canCatch = false;
+    private bool hasUsableItem;
     public GameObject catchable;
+    private Transform InitialTransform;
 
 	// Use this for initialization
 	void Start () {
@@ -17,20 +22,46 @@ public class HandController : MonoBehaviour {
 	void Update () {
 		if(canCatch == true && catchable != null)
         {
-            if (Input.GetKey(KeyCode.Mouse0))
+            if (Input.GetKey(KeyCode.Mouse0) && myUsableItem == null && catchable != null )
             {
-                catchable.gameObject.transform.SetParent(gameObject.transform);
+                InitialTransform = catchable.transform;
+                catchable.gameObject.transform.SetParent(HandHolder);
+                catchable.gameObject.transform.position = takenPos.position;
+                catchable.gameObject.transform.rotation = takenPos.rotation;
                 catchable.GetComponent<Collider>().isTrigger = true;
-                catchable.transform.position = takenPos.position;
-                catchable.transform.rotation = gameObject.transform.rotation;
+                catchable.GetComponent<Rigidbody>().useGravity = false;
+                try
+                {
+                    myUsableItem = catchable.GetComponent<GunController>() as IUsable;
+                }
+                catch(Exception ex)
+                {
+                  
+                }
             }            
+        }
+        if(myUsableItem != null && Input.GetKeyDown(KeyCode.Mouse0)){
+            myUsableItem.Use();
+        }
+
+        if(catchable != null && myUsableItem != null && Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            catchable.gameObject.transform.parent = null;
+            catchable.GetComponent<Collider>().isTrigger = false;
+            catchable.gameObject.GetComponent<Rigidbody>().useGravity = true;
+            myUsableItem.Throw();
+            catchable = null;
+            myUsableItem = null;
         }
 	}
 
     private void OnTriggerEnter(Collider other)
     {
-        canCatch = true;
-        catchable = other.gameObject;
+        if (other.gameObject.CompareTag("catchable"))
+        {
+            canCatch = true;
+            catchable = other.gameObject;
+        }
     }
 
     private void OnTriggerExit(Collider other)
